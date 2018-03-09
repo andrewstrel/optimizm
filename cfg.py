962ff5a362056285ca74d683c3c87fbffdd6983d
andrewstrel <sidbarret2006@gmail.com>
2018-03-09 18:55:32 +0300

    test

--- cfg.py
+++ cfg.py
@@ -1,6 +1,6 @@
 # list of variables
 import pandas as pd
 
-work_file = pd.DataFrame()
+work_file = pd.DataFrame() # create Pandas oject DataFrame
 list_w = [] # empty list for Listbox( number of waves)
 choise_wave = () # tuple of waves from Listbox
